785fad01f779514ceaaeded06bae66581dee9985
moabukar <mo-abukar10@hotmail.com>
2022-12-31 18:05:49 +0000

    add utils to randomise URLS

---
+++ utils/random.go
@@ -0,0 +1,11 @@
+package utils
+
+var runes = []rune("0123456789abcdefghijklmnopqrstuvwzyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandomURL(size int) string {
+	str := make([]rune, size)
+	
+	for 
+
+	return string(str)
+}
